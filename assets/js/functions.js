201c453bdcc9646de290eaeb109a9f8821c952c7
Saqib Razzaq <saqib.razzaq@virail.com>
2022-07-12 23:12:33 +0500

    basic command generation completed

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -8,21 +8,31 @@ function getSkeleton() {
     return JSON.parse(json);
 }
 
+function generateTextField(name, description, def) {
+    return `<input type="text" class="form-control" id="input_${name}" placeholder="${description}" value="${def}"></input>`;
+}
+
+function generateSelectField(name, description, options, def) {
+    let fieldHtml = `<select class="form-control" id="input_${name}">`;
+    
+    $.each(options, function(index, option) {
+        fieldHtml += `<option value="${option}">${option.toLowerCase()}</option>`;
+    });
+    
+    fieldHtml += '</select>';
+    
+    return fieldHtml;
+}
+
 function generateFieldHtml(field, options) {   
     fieldHtml = '';
 
     switch (options.type) {
         case 'text':
-            fieldHtml += `<input type="text" class="form-control" id="input_${field}" placeholder="${options.description}"></input>`;
+            fieldHtml += generateTextField(field, options.description, options.default);
             break;
         case 'select':
-            fieldHtml += `<select class="form-control" id="input_${field}">`;
-            
-            $.each(options.options, function(index, option) {
-                fieldHtml += `<option value="${option}">${option.toLowerCase()}</option>`;
-            });
-            
-            fieldHtml += '</select>';
+            fieldHtml += generateSelectField(field, options.description, options.options, options.default);
             break;
 
         default:
@@ -30,5 +40,23 @@ function generateFieldHtml(field, options) {
             break;
     }
     
-    return `<div class="col-12 mb-3"><label for="input_${field}" class="form-label">${options.description}</label>${fieldHtml}</div>`;;
+    return `<div class="col-4 mb-3"><label for="input_${field}" class="form-label">${options.description}</label>${fieldHtml}</div>`;;
+}
+
+function generateCommand(action, inputs) {
+    let command = `ffmpeg -y -i "input.ext" `;
+    
+    switch (action) {
+        case 'convert':
+            command += `-c:v ${inputs.vcodec} -c:a ${inputs.acodec} -b:v ${inputs.vbitrate} -b:a ${inputs.abitrate} -preset ${inputs.preset} -f ${inputs.output_format} -s ${inputs.output_resolution} -ss ${inputs.start_time} -to ${inputs.end_time} "${inputs.output_filename}"`;
+            break;
+        case 'flip':
+            command += `-vf "hflip" -c:v ${inputs.vcodec} -c:a ${inputs.acodec} -b:v ${inputs.vbitrate} -b:a ${inputs.abitrate} -preset ${inputs.preset} -f ${inputs.output_format} -s ${inputs.output_resolution} -ss ${inputs.start_time} -to ${inputs.end_time} "${inputs.output_filename}"`;
+            break;
+        default:
+            command = '';
+            break;
+    }
+
+    return command;
 }
